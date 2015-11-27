2b2566c812d060a9fe3f1cb4f6d50edbcd06c488
Hamun <commentatorbot@gmail.com>
2015-11-27 13:08:06 +0100

    The unit tests for the menu story has been set up

---
+++ PizzaWaiterServiceApp/UnitTests/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void GetAllMenues(int RestID)
+        {
+            RestDB restdb = new RestDB();
+
+            Rest rest = RestDB.GetByID(RestID);
+
+            Assert.IsNull(rest);
+            List<Menu> menues = Rest.Menues.OrderBy(menu => menu.Pos);
+            Assert.IsNull(menues);
+            foreach (Menu menu in menues)
+            {
+                Assert.IsNull(menu.Dishes);
+                //Style all the dishes
+                //print out title
+
+                foreach (Dish dish in menu.Dishes)
+                {
+                    Assert.IsNull(dish.Íngredients);
+                    //style them
+                    //print them out
+                    foreach (Ingredient i in dish.Ingredients)
+                    {
+
+                        //style them
+                        //print them out
+                    }
+                }
+
+            }
+
+        }
+    }
+}
