43914e9e325d3438ed01f524e95fe65c25f5adf3
lawura <lawura@niblosus.net>
2015-12-16 10:33:46 +0100

    Starting on Profile UI

---
+++ PizzaWaiterServiceApp/WebClient/Profile.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Templates/Global.Master" AutoEventWireup="true" CodeBehind="Profile.aspx.cs" Inherits="WebClient.Profile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Profile</h1>
+    <p>
+        <asp:Label ID="lblRank" runat="server" Text="Rank"></asp:Label></p>
+    <div id="divFavorites">
+        <h2>Favorites</h2>
+        <asp:CheckBoxList ID="cblFavorites" runat="server"></asp:CheckBoxList>
+        <asp:Button ID="btnOrderFavorites" runat="server" Text="Order" />
+        <p><asp:Literal ID="ltOrderFavoritesErrorMessage" runat="server"></asp:Literal></p> 
+        <h2>Discount card</h2>
+        <asp:Image ID="imgDiscountCard" runat="server" Width =" 300" Height="150" />
+        <p>Prepaid card: 
+            <asp:Literal ID="ltPrepaidAmount" runat="server"></asp:Literal></p>
+    </div>
+    <div id="divInfo">
+        <h2>User information</h2>
+        <p>Phone: 
+            <asp:Literal ID="ltPhone" runat="server"></asp:Literal>
+            <asp:TextBox ID="tbPhone" runat="server" Visible="False"></asp:TextBox>
+            <asp:Button ID="btnEditPhone" runat="server" Text="Change" />
+            <asp:Button ID="btnSavePhone" runat="server" Text="Save" Visible="False" />
+        </p>
+        
+        <asp:Repeater ID="rptAddresses" runat="server">
+            <ItemTemplate>
+                <asp:Literal ID="ltUserAddress" runat="server" Text='<%#Eval("UserAddress") %>'>'></asp:Literal>
+                <asp:ImageButton ID="ibtnDeleteAddress" runat="server" ImageUrl="~/Resources/button_delete_01.png" Width="24" Height="24" />
+            </ItemTemplate>
+            <SeparatorTemplate><br /></SeparatorTemplate>
+        </asp:Repeater>
+        <br />
+        <asp:TextBox ID="tbAddress" runat="server" Visible="False"></asp:TextBox>
+        <asp:Button ID="btnAddAddress" runat="server" Text="New" />
+        <asp:Button ID="btnCancelAddAdddress" runat="server" Text="Cancel" Visible="False" />
+        <asp:Button ID="btnSaveAddress" runat="server" Text="Save" Visible="False" />
+    </div>
+</asp:Content>
---
+++ PizzaWaiterServiceApp/WebClient/Profile.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebClient {
+    
+    public partial class Profile : System.Web.UI.Page {
+
+        private List<Address> Addresses;
+        private List<Favorite> Favorites;
+        private User User;
+        private int userID;
+        
+
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
+
+                //TODO: replace with session
+                // this.UserID = Session["UserID"];
+                // TODO: if no session or user doesnt exist, 
+                // then redirect to login
+          
+                // TODO: this.User = proxy.GetUserByID(this.UserID);
+                this.User = new User(1, "+45 7894563210");
+                
+                // TODO: Get data about addresses and favorites from server
+                // this.Addresses = proxy.GetAddressesByUserID(this.UserID);
+                // this.Favorites = proxy.GetFavoritesByUserID(this.UserID);
+                this.Addresses = new List<Address>();
+                this.Favorites = new List<Favorite>();
+
+                this.Addresses.Add(new Address(1, 1, "Address1, user1"));
+                this.Addresses.Add(new Address(2, 1, "Address2, user1"));
+                this.Addresses.Add(new Address(3, 1, "Address3, user1"));
+                //this.Addresses.Add(new Address(4, 2, "Address4, user2"));
+                //this.Addresses.Add(new Address(5, 2, "Address5, user2"));
+
+                this.Favorites.Add(new Favorite(1, 1, 1));
+                this.Favorites.Add(new Favorite(2, 1, 8));
+                //this.Favorites.Add(new Favorite(3, 2, 9));
+                //this.Favorites.Add(new Favorite(4, 2, 11));
+
+                //end todo
+                this.BindAddresses();
+                this.BindFavorites();
+            }
+        }
+
+        private void BindFavorites() {
+            
+        }
+
+        private void BindAddresses() {
+            this.rptAddresses.DataSource = this.Addresses;
+            this.rptAddresses.DataBind();
+        }
+
+        private void ToggleEditPhone() {
+        }
+        private void ToggleEditAddress() {
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+    public class Favorite
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int DishID { get; set; }
+
+        public Favorite(int id, int userId, int dishId) {
+            this.ID = id;
+            this.UserID = userId;
+            this.DishID = DishID;
+        }
+    }
+    public class Address
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string UserAddress { get; set; }
+
+        public Address(int id, int userID, string userAddress) {
+            this.ID = id;
+            this.UserID = UserID;
+            this.UserAddress = userAddress;
+        }
+    }
+
+    public class User {
+        public int ID { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(int userID, string phoneNumber) {
+            this.ID = userID;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
